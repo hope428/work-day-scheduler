4896dbad789f26134894bd61c2f7409c70536709
Leon Forsythe <lvforsythe95@gmail.com>
2022-11-16 18:42:26 -0500

    added logic for adding color classes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,39 @@
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
 
   var save = $(".saveBtn");
+  var currentTime = 15;
+  //creates array of all hour-* ids
+  var hours = [];
+  $.each($(".time-block"), function () {
+    hours.push($(this).attr("id"));
+  });
 
-  //creates array of all hour-* ids 
-  var hours = [] 
-    $.each($('.time-block'), function(){
-      hours.push($(this).attr('id'))
+  //add classes to divs
+  function addColorClass() {
+    //for each time-block, taking the number off the id attached
+    $.each($(".time-block"), function(index, hour){
+      var hourNumber = $(hour).attr('id')
+      hourNumber = hourNumber.slice(5)
+      
+      //comparing the timeblock hour to the current hour to assign class
+      if(hourNumber < currentTime){
+        $(hour).addClass('past')
+      } else if (hourNumber == currentTime){
+        $(hour).addClass('present')
+      } else {
+        $(hour).addClass('future')
+      }
     })
-
-  //saves input task to local storage 
+  }
+  //saves input task to local storage
   function saveEvent(event) {
     //targets the sibling of the selected button
     var selected = $(this).parent().attr("id");
@@ -50,4 +57,5 @@ $(function () {
 
   save.on("click", saveEvent);
   getEvents();
+  addColorClass()
 });
