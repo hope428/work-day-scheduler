806af818c87021d50a31f0ef10956b07b0b46522
Leon Forsythe <lvforsythe95@gmail.com>
2022-11-16 16:44:39 -0500

    created function to store task in local storage, also created function to retrieve tasks from local storage on page load

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -20,4 +17,39 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+
+  var save = $(".saveBtn");
+  var hours = [
+    "hour-9",
+    "hour-10",
+    "hour-11",
+    "hour-12",
+    "hour-13",
+    "hour-14",
+    "hour-15",
+    "hour-16",
+    "hour-17",
+  ];
+
+  function saveEvent(event) {
+    var selected = $(this).parent().attr("id");
+    var task = $(this).siblings("textarea").val().trim();
+
+    localStorage.setItem(selected, JSON.stringify(task));
+  }
+
+  //for each hour-*, retrieve local storage with hour-* key
+  function getEvents() {
+    if (localStorage) {
+      $.each(hours, function (index, hour) {
+        var retrievedTask = JSON.parse(localStorage.getItem(hour));
+        $("#" + hour)
+          .children("textarea")
+          .val(retrievedTask);
+      });
+    }
+  }
+
+  save.on("click", saveEvent);
+  getEvents();
 });
