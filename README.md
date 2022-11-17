dcef044f7fb6b9725f9f0cd9a46240e0d8f26a2a
Leon Forsythe <lvforsythe95@gmail.com>
2022-11-17 15:58:02 -0500

    updated README

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # work-day-scheduler
+
+## Description
+
+This project was created to help the user schedule their day for each hour
+of a 9-5 work day. The user can save events and clear all their events
+when finished with the day. The current day is also included in the top.
+
+Each time block is color coded, past is gray, present is red, and future is green. 
+
+## Usage 
+
+[Deployed URL](https://hope428.github.io/work-day-scheduler/) <br />
+[Github Repo](https://github.com/hope428/work-day-scheduler)
+
+![Deployed screenshot of application](./assets/images/screenshot.png)
+
+## Credits
+
+This project uses Bootstrap and the DayJS library
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,10 @@
 $(function () {
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  //
-  // TODO: Add code to display the current date in the header of the page.
-
   var date = dayjs()
   var displayDate = date.format('dddd, MMMM D[th]')
   var currentTime = parseInt(date.format('HH'))
   var dayDisplayEl = $('#currentDay')
   var save = $(".saveBtn");
+  var clearBtn = $('#clear')
 
   dayDisplayEl.text(displayDate)
   //creates array of all hour-* ids
@@ -59,6 +51,12 @@ $(function () {
     }
   }
 
+  //clears all tasks and calls getEvents again to rerender
+  clearBtn.on("click", function(){
+    localStorage.clear()
+    getEvents()
+  })
+  
   save.on("click", saveEvent);
   getEvents();
   addColorClass()
